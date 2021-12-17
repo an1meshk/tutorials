2a2aad5ec274b3b959a6816051aa413d56a03539
AK DevCraft <6288619+an1meshk@users.noreply.github.com>
2021-12-17 09:54:25 -0600

    Update App.js

--- react-runtime-variables/src/App.js
+++ react-runtime-variables/src/App.js
@@ -15,7 +15,7 @@ function App() {
         <h1>{RUNTIME_ENV_KEY}</h1>
         <a
           className="App-link"
-          href="https://dev.to/an1meshk/react-runtime-variables-49dc"
+          href="https://dev.to/akdevcraft/react-runtime-variables-49dc"
           target="_blank"
           rel="noopener noreferrer"
         >
