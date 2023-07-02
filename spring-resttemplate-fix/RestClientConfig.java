9c1ec2bbf3cb1f7861849864cc8a47c088ac9b51
AK DevCraft <6288619+an1meshk@users.noreply.github.com>
2023-07-02 18:53:45 -0500

    Create RestClientConfig.java

---
+++ spring-resttemplate-fix/RestClientConfig.java
@@ -0,0 +1,22 @@
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    public CloseableHttpClient httpClient() {
+        return HttpClientBuilder.create()
+                .setMaxConnTotal(100)      // Set maximum total connections
+                .setMaxConnPerRoute(20)    // Set maximum connections per route
+                .build();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient()));
+    }
+}
