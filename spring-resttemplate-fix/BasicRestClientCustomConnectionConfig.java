86afb83b46d03b64ee6b7b3ed88d06ccd425ce8b
AK DevCraft <6288619+an1meshk@users.noreply.github.com>
2023-07-02 19:06:19 -0500

    Update BasicRestClientCustomConnectionConfig.java

--- spring-resttemplate-fix/BasicRestClientCustomConnectionConfig.java
+++ spring-resttemplate-fix/BasicRestClientCustomConnectionConfig.java
@@ -3,6 +3,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 @Configuration
 public class BasicRestClientCustomConnectionConfig {
