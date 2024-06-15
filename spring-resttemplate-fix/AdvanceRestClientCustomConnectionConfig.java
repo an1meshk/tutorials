07a7230064d4bdd8c4757e4dda0a9aec4cb7c4ea
AK DevCraft <6288619+an1meshk@users.noreply.github.com>
2024-06-14 17:33:48 -0700

    fix typo

--- spring-resttemplate-fix/AdvanceRestClientCustomConnectionConfig.java
+++ spring-resttemplate-fix/AdvanceRestClientCustomConnectionConfig.java
@@ -2,7 +2,7 @@ import java.util.concurrent.TimeUnit;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.conn.PoolingHtppClientConnectionManager;
+import org.apache.http.client.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,13 +12,13 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 public class AdvanceRestClientCustomConnectionConfig {
 
     @Bean
-    public PoolingHtppClientConnectionManager customizedPoolingHtppClientConnectionManager(){
+    public PoolingHttpClientConnectionManager customizedPoolingHttpClientConnectionManager(){
       /*
         Setting the connection caching time to 5 minutes, so that the connection is in an open state for 5 mins. 
         This will add some extra time as every 5 mins SSL handshake will happen. However, software is all about trade-off,
         and here we are making sure, we don't keep calling cached API DNS
       */
-      PoolingHtppClientConnectionManager connManager = new PoolingHtppClientConnectionManager(5, TimeUnit.MINUTES);
+      PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(5, TimeUnit.MINUTES);
       connManager.setMaxTotal(100);    // Set maximum total connections
       connManager.setDefaultMaxPerRoute(20);     // Set maximum connections per route
       return connManager;
@@ -33,7 +33,7 @@ public class AdvanceRestClientCustomConnectionConfig {
                                   .build();
 
         HttpClientBuilder clientBuilder = HttpClientBuilder.create()
-                                  .setConnectionManager(customizedPoolingHtppClientConnectionManager)
+                                  .setConnectionManager(customizedPoolingHttpClientConnectionManager)
                                   .setConnectionManagerShared(true)  //this is important to set as true in case of more than one downstream APIs as we want to set a common HTTP connection pool
                                   .setDefaultRequestConfig(reqConfig);
 
